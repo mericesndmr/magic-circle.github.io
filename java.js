a8cc57b2f510820b62032605cfba7bb7024e58be
mericesndmr <altugmerice@gmail.com>
2022-03-07 20:28:19 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,6 @@
+document.getElementById("circle").onclick=function() {
+  document.getElementById("circle").style.display="none";
+}
+
+
+// document.getElementById metodu ile id özelliği belirttiğim elementin referansının alınmasını sağladım.
